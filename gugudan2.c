a8a91bd86fd922fe0bb920c0c46493f28c5d8d89
Dongju <wjdehdwn2475@naver.com>
2023-11-22 16:22:51 +0900

    리눅수 수업

---
+++ gugudan2.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int child_id;
+
+void intHandler();
+void intHandler2();
+void intHandler3();
+
+void printf_gugu(){
+	for(int i=0;i<=20;i++){
+		for(int j=1;j<20;j++){
+			printf("%d X %d = %d\n",i,j, i*j);
+		}
+		if(i==9)
+			signal(SIGINT,intHandler3);
+	}
+}
+
+int main(){
+	int pid;
+	if((pid=fork()) == 0){
+		signal(SIGINT,intHandler2);
+		printf_gugu();
+		pause();
+	}
+	else{
+		child_id = pid;
+		signal(SIGINT, intHandler);
+		wait();
+	}
+}
+
+void intHandler(){
+	kill(child_id,SIGINT);
+}
+
+void intHandler2(){
+	
+}
+
+void intHandler3(){
+	kill(-getppid(),SIGKILL);
+}
